e56285f2079e93979a1b61549fe8a3ae3b9a2785
Alex Young <alex.young@hive.hr>
2019-08-06 21:57:58 +0100

    Add new app string

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require("path");
 const strings = {
     "app/message-one": "hello",
     "app/message-two": "goodbye",
-    "app/message-three": "my name is alex"
+    "app/message-three": "my name is alex",
+    "app/login": "Do a login"
 };
 
 fs.writeFileSync(
