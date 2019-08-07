23065ef2d229087c473e13387c621b63c688458c
Alex Young <alex.young@hive.hr>
2019-08-07 15:46:54 +0100

    Add new app string (#5)
    * Add new app string
    
    * Extract new messages

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const strings = {
     "app/message-one": "hello",
     "app/message-two": "goodbye",
     "app/message-three": "my name is alex",
+    "app/login": "Do a login",
     "app/message-four": "emma smells"
 };
 
